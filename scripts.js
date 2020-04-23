e81099cad0ad3a3e31cde4a39c7597cd53a1e0e2
Tommy75hu <tommy4coding@gmail.com>
2020-04-23 17:51:46 +0200

    third. i'm lost...

--- scripts.js
+++ scripts.js
@@ -4,6 +4,33 @@ console.log(name + "'s apples");
 console.log('Hello world')
 
 $('h1').text('We are having a 10 min break')
+$('p').html('Stay at home coding camp, Likes:<strong>13</strong>');
+
+$('strong').css('background', 'lime');
+$('h2').toggleClass('blue');
+
+
+$('ul').append('<li>My name is ' + name + '</li>');
+
+$('#add').click(() => {
+    $('ul').append('<li>My name is ' + name + '</li>');
+});
+
+$('#remove').click(() => {
+    $('li:last-child').remove();
+});
+
+let age=2;
+
+console.log('Starting the if');
+
+if (age < 18) {
+console.log('Not old enough to drink');
+} else {
+console.log('Old enozgh to drink vodka');
+}
+
+console.log('Thisis after the if');
 
 // This is a java script comment
 
